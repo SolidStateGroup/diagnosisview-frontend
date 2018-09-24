fa6fc5a22b6a33f69ede6c9efbf3d3ca77cc85fd
andrewmoffatt <andrew@solidstategroup.com>
2018-09-24 17:05:12 +0100

    Card-28 Nice category logo

--- mobile/app/project/constants.js
+++ mobile/app/project/constants.js
@@ -1,6 +1,8 @@
 module.exports = {
   linkIcons: {
     'NHS_CHOICES': require('../images/nhs-choices.png'),
-    'MEDLINE_PLUS': require('../images/logo-medlineplus.png')
+    'MEDLINE_PLUS': require('../images/logo-medlineplus.png'),
+    'NICE_CKS': require('../images/nice.png'),
+    'CUSTOM': require('../images/default-link.png'),
   }
 }
