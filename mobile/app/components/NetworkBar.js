5e88ddbea58bba46246f6ba84a29ea9627212d5c
Luke Fanning <luke@solidstategroup.com>
2018-09-25 14:40:24 +0100

    Improved db is down detection

--- mobile/app/components/NetworkBar.js
+++ mobile/app/components/NetworkBar.js
@@ -1,42 +1,22 @@
-module.exports = class extends React.Component {
-	state = {};
-	heartbeat = () => {
-		fetch(Project.api.substr(0, Project.api.length - 4) + 'public/status')
-			.then(res => {
-				this.setState({dbIsDown: !res.ok})
-			})
-			.catch(e => this.setState({dbIsDown: true}));
-	}
-	componentDidMount() {
-		// Set up heartbeat to server every 10 secs
-		this.heartbeat();
-		this.interval = setInterval(this.heartbeat, 10 * 1000);
-	}
-	componentWillUnmount() {
-		clearInterval(this.interval);
-	}
-	render() {
-		return (
-			<NetworkProvider>
-				{(isConnected) => (
-					<View>
-						<SlideUp duration={200}
-								style={ [{backgroundColor: pallette.third}]}
-								value={!isConnected || this.state.dbIsDown} height={64}>
-							<Row>
-								<Column>
-									<ION style={{fontSize: em(2), color: 'white'}} name="md-warning"/>
-								</Column>
-								<Column style={{width: DeviceWidth - 100}}>
-									<Text style={[Styles.barText]}>
-										{this.props.message || "The server connection appears to be down. Please try again later."}
-									</Text>
-								</Column>
-							</Row>
-						</SlideUp>
-					</View>
-				)}
-			</NetworkProvider>
-		)
-	}
-};
+module.exports = (props) => (
+	<NetworkProvider>
+		{(isConnected, isDBDown) => (
+			<View>
+				<SlideUp duration={200}
+						style={ [{backgroundColor: pallette.third}]}
+						value={!isConnected || isDBDown} height={64}>
+					<Row>
+						<Column>
+							<ION style={{fontSize: em(2), color: 'white'}} name="md-warning"/>
+						</Column>
+						<Column style={{width: DeviceWidth - 100}}>
+							<Text style={[Styles.barText]}>
+								{props.message || "The server connection appears to be down. Please try again later."}
+							</Text>
+						</Column>
+					</Row>
+				</SlideUp>
+			</View>
+		)}
+	</NetworkProvider>
+)
