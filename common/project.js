8e66fb86684ac98522bf68cfb1c6af5327f44ac1
kyle-ssg <kyle@solidstategroup.com>
2021-10-06 14:41:53 +0100

    Add institution

--- common/project.js
+++ common/project.js
@@ -2,5 +2,6 @@ module.exports = {
     debug: true,
     api: 'https://api-staging.diagnosisview.org/api/',
     public: 'https://api-staging.diagnosisview.org/public/',
+    base: 'https://api-staging.diagnosisview.org/public/',
     ga: ''
 };
