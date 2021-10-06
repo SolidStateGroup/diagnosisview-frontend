608d19f196cdeb80ab3f0eab39200cc0ea9a9bd5
kyle-ssg <kyle@solidstategroup.com>
2021-10-06 14:08:21 +0100

    dashboard nav

--- web/routes.js
+++ web/routes.js
@@ -12,12 +12,22 @@ import ManageLinkLogosPage from './components/pages/ManageLinkLogosPage';
 import DiagnosisDetailPage from './components/pages/DiagnosisDetailPage';
 import NotFoundPage from './components/pages/NotFoundPage';
 import DashboardPage from "./components/pages/DashboardPage";
+import DashboardAccountPage from "./components/pages/DashboardAccountPage";
+import DashboardSearchPage from "./components/pages/DashboardSearchPage";
+import DashboardHistoryPage from "./components/pages/DashboardHistoryPage";
+import DashboardFavouritesPage from "./components/pages/DashboardFavouritesPage";
+import DashboardFeedbackPage from "./components/pages/DashboardFeedbackPage";
 
 export default (
     <App>
         <Switch>
             <Route path="/" exact component={HomePage}/>
             <Route path="/dashboard" exact component={DashboardPage}/>
+            <Route path="/dashboard/account" exact component={DashboardAccountPage}/>
+            <Route path="/dashboard/search" exact component={DashboardSearchPage}/>
+            <Route path="/dashboard/history" exact component={DashboardHistoryPage}/>
+            <Route path="/dashboard/favourites" exact component={DashboardFavouritesPage}/>
+            <Route path="/dashboard/feedback" exact component={DashboardFeedbackPage}/>
             <Route path="/admin/codes" exact component={ManageCodesPage}/>
             <Route path="/admin/users" exact component={ManageUsersPage}/>
             <Route path="/admin/institutions" exact component={InstitutionsPage}/>
