a0dc19bf3334593ad05ac6b890b3d58cd0b601cb
kyle-ssg <kyle@solidstategroup.com>
2021-10-15 14:13:22 +0100

    Dashboard codes page

--- web/components/ResultListItem.js
+++ web/components/ResultListItem.js
@@ -6,7 +6,7 @@ import Tag from "./Tag"; // we need this to make JSX compile
 const TheComponent = ({result}) => {
     return (
         <ListItem className="list-item--no-hover list-item--no-pad">
-            <Link to={"/codes/"+result.code}>
+            <Link to={AccountStore.model? "/dashboard/codes/"+result.code:"/codes/"+result.code}>
                 <Row>
                     <div className="mr-2">{result.friendlyName}</div>
                     {_.map(result.tags, tag => (
